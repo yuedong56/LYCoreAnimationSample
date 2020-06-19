e6c6d2f85f5c14c5f448a95e6c9565b9c5478ad3
yuedong56 <yuedong56@126.com>
2020-06-19 16:59:38 +0800

    暂停恢复

--- LYCoreAnimationSample/CABasicAnimation/BasicAnimationController.m
+++ LYCoreAnimationSample/CABasicAnimation/BasicAnimationController.m
@@ -32,6 +32,12 @@
     self.layer.bounds = CGRectMake(0, 0, 100, 120);
     
     [self.view.layer addSublayer:self.layer];
+    
+    UIButton *pauseButton = [UIButton buttonWithType:UIButtonTypeSystem];
+    [pauseButton setTitle:@"暂停/恢复" forState:UIControlStateNormal];
+    pauseButton.frame = CGRectMake(150, 100, 80, 40);
+    [pauseButton addTarget:self action:@selector(pauseButtonAction:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:pauseButton];
 }
 
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
@@ -54,7 +60,7 @@
     }
     
     {   // 旋转动画
-        CABasicAnimation *tAnim = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+        CABasicAnimation *tAnim = [CABasicAnimation animationWithKeyPath:@"transform.rotation.y"];
         tAnim.fromValue = @(0);
         tAnim.toValue = @(M_PI_2 *4);
         tAnim.duration = 2;
@@ -66,6 +72,31 @@
         //self.layer.anchorPoint = CGPointMake(1, 1); //旋转动画是以锚点为中心旋转的
 
         [self.layer addAnimation:tAnim forKey:@"k_Tansform_Animation_Key"];
+    }
+}
+
+#pragma mark -
+- (void)pauseButtonAction:(UIButton *)button
+{
+    CAAnimation *animation = [self.layer animationForKey:@"k_Position_Animation_Key"];
+    if (animation) {
+        if (self.layer.speed == 0) {//重启
+            //获得暂停的时间
+            CFTimeInterval beginTime= CACurrentMediaTime() - self.layer.timeOffset;
+            //设置偏移量
+            self.layer.timeOffset = 0;
+            //设置开始时间
+            self.layer.beginTime = beginTime;
+            //设置动画速度，开始运动
+            self.layer.speed = 1.0;
+        } else {//暂停
+            //取得指定图层动画的媒体时间，后面参数用于指定子图层，这里不需要
+            CFTimeInterval interval = [self.layer convertTime:CACurrentMediaTime() fromLayer:nil];
+            //设置时间偏移量，保证暂停时停留在旋转的位置
+            self.layer.timeOffset = interval;
+            //速度设置为0，暂停动画
+            self.layer.speed = 0;
+        }
     }
 }
 
